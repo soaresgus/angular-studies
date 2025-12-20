157fca8b7611c9cb6db02cb8606620b3d0a614da
Gustavo Soares <gustavosoaresbt@hotmail.com>
2025-12-20 16:48:42 -0300

    20-12-25: module 1 finished

---
+++ rocketseat/modules/1/first-project/src/app/comp-1/comp-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-1',
+  imports: [],
+  templateUrl: './comp-1.component.html',
+  styleUrl: './comp-1.component.css'
+})
+export class Comp1Component {
+
+}
